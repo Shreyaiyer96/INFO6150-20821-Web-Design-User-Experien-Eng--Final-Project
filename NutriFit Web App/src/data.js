6423e274de7d29a8061b0695c09edd2db6fef707
Tanmay Shekhar <81084072+tanmayshekhar07@users.noreply.github.com>
2022-12-07 16:56:23 -0500

    membership content added

--- NutriFit Web App/src/data.js
+++ NutriFit Web App/src/data.js
@@ -205,19 +205,19 @@ export const plans = [
         desc: 'This package is perfect for beginners who need constant help',
         price: 29.99,
         features: [
-            {feature: 'First Feature', available: true},
-            {feature: 'Second Feature', available: true},
-            {feature: 'Third Feature', available: true},
-            {feature: 'Fourth Feature', available: true},
-            {feature: 'Fifth Feature', available: true},
-            {feature: 'Fifth Feature Plus', available: false},
-            {feature: 'Sixth Feature', available: false},
-            {feature: 'Seventh Feature', available: false},
-            {feature: 'Seventh Feature Plus', available: false},
-            {feature: 'Eighth Feature', available: false},
-            {feature: 'Ninth Feature', available: false},
-            {feature: 'Tenth Feature', available: false},
-            {feature: 'Eleventh Feature', available: false}
+            {feature: '1200+ at-home workouts', available: true},
+            {feature: 'dance', available: true},
+            {feature: 'yoga', available: true},
+            {feature: '30+ goal based fitness programs', available: true},
+            {feature: 'Stamina and mobility', available: true},
+            {feature: 'health podcasts', available: false},
+            {feature: 'Cross fit', available: false},
+            {feature: 'All PRO gyms', available: false},
+            {feature: 'At centre group classes', available: false},
+            {feature: 'Meditation sessions', available: false},
+            {feature: 'Free 2 month extension', available: false},
+            {feature: '4 Sessions/month at ELITE gyms', available: false},
+            {feature: 'one free gym apparel', available: false}
         ]
     },
     {
@@ -226,19 +226,19 @@ export const plans = [
         desc: 'This is the perfect package for beginners who know what their doing',
         price: 49.99,
         features: [
-            {feature: 'First Feature', available: true},
-            {feature: 'Second Feature', available: true},
-            {feature: 'Third Feature', available: true},
-            {feature: 'Fourth Feature', available: true},
-            {feature: 'Fifth Feature', available: true},
-            {feature: 'Fifth Feature Plus', available: true},
-            {feature: 'Sixth Feature', available: true},
-            {feature: 'Seventh Feature', available: true},
-            {feature: 'Seventh Feature Plus', available: true},
-            {feature: 'Eighth Feature', available: false},
-            {feature: 'Ninth Feature', available: false},
-            {feature: 'Tenth Feature', available: false},
-            {feature: 'Eleventh Feature', available: false}
+            {feature: '1200+ at-home workouts', available: true},
+            {feature: 'dance', available: true},
+            {feature: 'yoga', available: true},
+            {feature: '30+ goal based fitness programs', available: true},
+            {feature: 'Stamina and mobility', available: true},
+            {feature: 'health podcasts', available: true},
+            {feature: 'Cross fit', available: true},
+            {feature: 'All PRO gyms', available: true},
+            {feature: 'At centre group classes', available: true},
+            {feature: 'Meditation sessions', available: false},
+            {feature: 'Free 2 month extension', available: false},
+            {feature: '4 Sessions/month at ELITE gyms', available: false},
+            {feature: 'one free gym apparel', available: false}
         ]
     },
     {
@@ -247,19 +247,19 @@ export const plans = [
         desc: 'This package is perfect for busy people who need home service',
         price: 89.99,
         features: [
-            {feature: 'First Feature', available: true},
-            {feature: 'Second Feature', available: true},
-            {feature: 'Third Feature', available: true},
-            {feature: 'Fourth Feature', available: true},
-            {feature: 'Fifth Feature', available: true},
-            {feature: 'Fifth Feature Plus', available: true},
-            {feature: 'Sixth Feature', available: true},
-            {feature: 'Seventh Feature', available: true},
-            {feature: 'Seventh Feature Plus', available: true},
-            {feature: 'Eighth Feature', available: true},
-            {feature: 'Ninth Feature', available: true},
-            {feature: 'Tenth Feature', available: true},
-            {feature: 'Eleventh Feature', available: true}
+            {feature: '1200+ at-home workouts', available: true},
+            {feature: 'dance', available: true},
+            {feature: 'yoga', available: true},
+            {feature: '30+ goal based fitness programs', available: true},
+            {feature: 'Stamina and mobility', available: true},
+            {feature: 'health podcasts', available: true},
+            {feature: 'Cross fit', available: true},
+            {feature: 'All PRO gyms', available: true},
+            {feature: 'At centre group classes', available: true},
+            {feature: 'Meditation sessions', available: true},
+            {feature: 'Free 2 month extension', available: true},
+            {feature: '4 Sessions/month at ELITE gyms', available: true},
+            {feature: 'one free gym apparel', available: true}
         ]
     }
 ]
