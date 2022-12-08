6cb6866580aed90cfc386c76872cd7eb4be1aa96
Shreyaiyer96 <iyer.shre@northeastern.edu>
2022-12-08 01:44:02 -0500

    diet plans added

--- NutriFit Web App/src/App.jsx
+++ NutriFit Web App/src/App.jsx
@@ -4,10 +4,10 @@ import React from "react";
 import Home from "./pages/home/Home";
 import About from "./pages/about/About";
 import Contact from "./pages/contact/Contact";
-import Gallery from "./pages/gallery/Gallery";
+//import Gallery from "./pages/gallery/Gallery";
 import NotFound from "./pages/notFound/NotFound";
 import Plans from "./pages/plans/Plans";
-import Trainers from "./pages/trainers/Trainers";
+import DietPlan from "./pages/trainers/Trainers";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Yoga from "./pages/newPages/Yoga";
@@ -57,7 +57,7 @@ const App = () => {
 				<Route path="contact" element={<Contact />} />
 				<Route path="Products" element={<Products products={ products } />} />
 				<Route path="plans" element={<Plans />} />
-				<Route path="trainers" element={<Trainers />} />
+				<Route path="trainers" element={<DietPlan />} />
 				<Route path="login" element={<Login />} />
 				<Route path="newPages" element={<Yoga />} />
 				<Route path="kickboxing" element={<Kickboxing />} />
--- NutriFit Web App/src/pages/trainers/Trainers.jsx
+++ NutriFit Web App/src/pages/trainers/Trainers.jsx
@@ -1,43 +1,78 @@
-import React from "react";
-import Header from "../../components/Header";
-import HeaderImage from "../../images/header_bg_5.jpg";
-import { BsInstagram } from "react-icons/bs";
-import { AiOutlineTwitter } from "react-icons/ai";
-import { FaLinkedinIn } from "react-icons/fa";
-import { FaFacebookF } from "react-icons/fa";
-import { trainers } from "../../data";
-import Trainer from "../../components/Trainer";
+// import React from "react";
+// import Header from "../../components/Header";
+// import HeaderImage from "../../images/header_bg_5.jpg";
+// import { BsInstagram } from "react-icons/bs";
+// import { AiOutlineTwitter } from "react-icons/ai";
+// import { FaLinkedinIn } from "react-icons/fa";
+// import { FaFacebookF } from "react-icons/fa";
+// import { trainers } from "../../data";
+// import Trainer from "../../components/Trainer";
 import "./trainers.css";
 
-const Trainers = () => {
-	return (
-		<>
-			<Header image={HeaderImage} title="Our Trainers">
-				Adipisicing labore laboris ea sunt cillum ea velit.Adipisicing labore la
-				boris ea sunt cillum ea velit. sunt cillum ea velit.
-			</Header>
-			<section className="trainers">
-				<div className="container trainers__container">
-					{trainers.map(({ id, image, name, job, socials }) => {
-						return (
-							<Trainer
-								key={id}
-								image={image}
-								name={name}
-								job={job}
-								socials={[
-									{ icon: <BsInstagram />, link: socials[0] },
-									{ icon: <AiOutlineTwitter />, link: socials[1] },
-									{ icon: <FaFacebookF />, link: socials[2] },
-									{ icon: <FaLinkedinIn />, link: socials[3] },
-								]}
-							/>
-						);
-					})}
-				</div>
-			</section>
-		</>
-	);
+// const Trainers = () => {
+// 	return (
+// 		<>
+// 			<Header image={HeaderImage} title="Our Trainers">
+// 				Adipisicing labore laboris ea sunt cillum ea velit.Adipisicing labore la
+// 				boris ea sunt cillum ea velit. sunt cillum ea velit.
+// 			</Header>
+// 			<section className="trainers">
+// 				<div className="container trainers__container">
+// 					{trainers.map(({ id, image, name, job, socials }) => {
+// 						return (
+// 							<Trainer
+// 								key={id}
+// 								image={image}
+// 								name={name}
+// 								job={job}
+// 								socials={[
+// 									{ icon: <BsInstagram />, link: socials[0] },
+// 									{ icon: <AiOutlineTwitter />, link: socials[1] },
+// 									{ icon: <FaFacebookF />, link: socials[2] },
+// 									{ icon: <FaLinkedinIn />, link: socials[3] },
+// 								]}
+// 							/>
+// 						);
+// 					})}
+// 				</div>
+// 			</section>
+// 		</>
+// 	);
+// };
+
+// export default Trainers;
+
+
+
+import 'bootstrap'
+import "bootstrap/dist/css/bootstrap.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import "remixicon/fonts/remixicon.css";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import React from "react";
+import { Fragment } from "react";
+//import Header from "../components-diet_plan/header/Header";
+import HeroSlider from "../../components-diet_plan/hero-slider/HeroSlider";
+import PopularMenu from "../../components-diet_plan/popular-menu/PopularMenu";
+import ChooseUs from "../../components-diet_plan/choose-us/ChooseUs";
+import MenuPack from "../../components-diet_plan/menu-pack/MenuPack";
+import Testimonials from "../../components-diet_plan/testimonials/Testimonials";
+import Download from "../../components-diet_plan/download-section/Downlaod";
+//import Footer from "../../components-diet_plan/footer/Footer";
+
+
+const DietPlan = () => {
+
+    return <Fragment>
+        <HeroSlider/>
+        <PopularMenu/>
+        <ChooseUs/>
+        <MenuPack/>
+        <Testimonials/>
+        <Download/>
+        </Fragment>
 };
 
-export default Trainers;
+export default DietPlan;
